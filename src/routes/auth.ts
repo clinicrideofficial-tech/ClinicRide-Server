9570acb02b24b3d7e6135b258f09646d3552aae0
neeraj <neerajkomatishetti@gmail.com>
2026-01-25 16:45:02 +0530

    update env

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -93,7 +93,9 @@ router.get("/google/callback", async (req, res) => {
 
       res.cookie("token", token, {
         httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        secure: true,    // Required for cross-site cookies
+        sameSite: "none", // Required for cross-site cookies
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
       // Redirect to frontend callback handler
